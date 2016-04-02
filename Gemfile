5e80a05f181212e72ccc63780caa9b6ff7941349
Kyle Schmolze <kyletns@gmail.com>
2016-04-01 17:37:13 -0700

    prep for heroku

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 group :development, :test do
+  gem 'dotenv-rails'
   gem 'byebug'
 end
 group :development do
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :google_oauth2,
+           Rails.application.secrets.omniauth_provider_key,
+           Rails.application.secrets.omniauth_provider_secret,
+           scope: 'email,https://www.googleapis.com/auth/drive'
 end
