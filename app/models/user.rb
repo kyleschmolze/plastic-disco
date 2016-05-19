7d0e6599c297b4e9ab405b6d87ac8f84dcb16f95
Kyle Schmolze <kyletns@gmail.com>
2016-05-19 09:02:12 -0700

    wrote first set of ultianalytics import stuff, looks good works good but timestamps are confusing and i havent decided on a schema for our end yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,4 +42,47 @@ class User < ActiveRecord::Base
       end
     end
   end
+
+  def self.import_for_user
+    # most of the integer timestamps are stored as seconds since Jan 1, 2001!
+    beginning_of_time = Date.parse("Jan 1 2001 UTC").beginning_of_day
+
+    response = HTTParty.get 'http://www.ultianalytics.com/rest/view/team/5699535384870912/gamesdata'
+    games = JSON.parse response.body
+    action_types = []
+    for game in games
+      tournament = game['tournamentName']
+      opponent = game['opponentName']
+      game_id = game['gameId']
+      our_final_score = game['ours']
+      their_final_score = game['theirs']
+      game_start_time = Time.at game['msSinceEpoch']/1000
+      puts "GAME start time:  #{game_start_time}"
+
+      points = JSON.parse game['pointsJson']
+
+      for point in points
+        our_score = point['summary']['score']['ours']
+        their_score = point['summary']['score']['theirs']
+        duration = point['summary']['elapsedTime']
+        start_time = beginning_of_time + point['startSeconds'].seconds
+        end_time = beginning_of_time + point['endSeconds'].seconds
+        line_type = point['summary']['lineType']
+        puts "Point start time: #{start_time}"
+
+        for event in point['events']
+          action = event['action']
+          action_types << action
+          event['type'] # 'Offense' or 'Defense'
+          thrower = event['passer']
+          receiver = event['receiver']
+          start_time = beginning_of_time + event['timestamp'].seconds
+          puts "Event start time: #{start_time}"
+        end
+      end
+    end
+    puts action_types.uniq
+  end
+
+
 end
