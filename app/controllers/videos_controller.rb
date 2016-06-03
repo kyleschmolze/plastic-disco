15f65b81d4a6128e6867f4db397c0d17defe60de
kyletns <kyletns@gmail.com>
2016-06-02 20:52:06 -0700

    sort videos by name

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.includes(:events).order('starts_at ASC').paginate(page: params[:page], per_page: 100)
+    @videos = Video.includes(:events).order('title ASC').paginate(page: params[:page], per_page: 100)
   end
 
   def show
